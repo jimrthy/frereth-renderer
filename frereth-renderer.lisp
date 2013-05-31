5b138f55e6726cfa19aa2ea6bfbc1e51b86ca3f6
James Ashley <james@teamdaves.us>
2013-05-30 21:43:59 -0500

    Shift communication stubs over to frereth-communicator, where they belong

--- frereth-renderer.lisp
+++ frereth-renderer.lisp
@@ -9,20 +9,12 @@
 	      "=> "))
   (read))
 
-(defun build-repl-y-client (port)
-  (error 'not-implemented))
-
 (defvar *client* (build-repl-y-client *client-port*))
-
-(defun send-to-repl-y-client (fn &rest args)
-  (format t "Do something over *client*")
-  ;; As soon as I get a clue what that might be...
-  (error 'not-implemented))
 
 (defun frereth (form)
   "Basic wrapper over the clojure interface. Send [a] form over nrepl.
 Obviously horribly unsafe."
-  (send-to-repl-y-client form))
+  (send-to-repl-y-client *client* form))
 
 (defun main (&optional args)
   (declare (ignore args))
--- package.lisp
+++ package.lisp
@@ -7,6 +7,7 @@
 (defpackage #:frereth-renderer
   (:use #:cl)
   (:export #:main)
-  (:import-from #:frereth-renderer-configuration :*client-port*))
+  (:import-from #:frereth-renderer-configuration :*client-port*)
+  (:import-from #:frereth-communicator #:build-repl-y-client #:send-to-repl-y-client))
 
 
